f676147c781a39dc259edf21b5c019e35aa37622
Nora Liechty <e227540@edu.sbl.ch>
2023-10-30 12:01:23 +0100

    Aufgabe 1 und 2 gelöst

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+
+  if (currentElement === 'e' ) {
+    result.push("")
+  } else if (currentElement === 'E') {
+    result.push("")
+  }
+else {
     result.push(currentElement)
+  }
+  }
+  return result.join("")
+}
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    //Wandle das aktuelle Element in einen Grossbuchstaben um 
+    const capitalizedElement = currentElement.toUpperCase()
+    
+    //Hänge den Grossbuchstaben an das Resultat an...
+    result.push(capitalizedElement)
+
   }
   return result.join("")
 }
