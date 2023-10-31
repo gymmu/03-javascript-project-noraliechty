f9d781e06a38625009da9254a145a93cf384632e
Nora Liechty <e227540@edu.sbl.ch>
2023-10-31 09:26:45 +0100

    Aufgabe 5 fast vollständig

--- script.js
+++ script.js
@@ -46,11 +46,12 @@ export function aufgabe03(args) {
     if (currentElement === 'e') {
      count = count + 1
     }
-    //Zähle auch alle 'E's die enthalten sind
+    {//Zähle auch alle 'E's die enthalten sind
     if (currentElement === 'E') {
     count = count + 1 
     }
 
+  }
   }
   return count
 }
@@ -58,9 +59,38 @@ export function aufgabe03(args) {
 export function aufgabe04(args) {
   const input = args
   const result = []
+  let count = 0 
+
+  for (let i = 0; i < input.length; i++) {
+    const countWords = input[i]
+  //Zähle alle Wörter die enthalten sind
+
+  }
+  return 2
+}
+
+export function aufgabe05(args) {
+  const input = args
+  const result = [] 
+let capitalLetters = 0 
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    //Gib an ob keine Grossbuchstaben enthalten sind
+
+    if ( currentElement === ".") {
+      //do nothing
+    } else if (currentElement === " ") {
+    } else if (currentElement === currentElement.toUpperCase()) {
+    capitalLetters++
+    }
+  }
   
+
+if (capitalLetters > 0) {
+  return true 
+} else {
+  return false
 }
+
 }
